d0982bd6ff7cf2e294303381de83df93a8426428
Dustin Schau <dustinschau@gmail.com>
2019-07-23 10:21:07 -0700

    chore: force a rebuild

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -58,7 +58,9 @@ module.exports = {
     "Mdx.frontmatter.author": `AuthorYaml`,
   },
   plugins: [
-    // https://github.com/gatsbyjs/gatsby/issues/11961#issuecomment-492893594
+    /*
+     * https://github.com/gatsbyjs/gatsby/issues/11961#issuecomment-492893594
+     */
     `gatsby-plugin-remove-fingerprints`,
     {
       resolve: `gatsby-source-npm-package-search`,
